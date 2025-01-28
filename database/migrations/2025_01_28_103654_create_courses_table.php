2f176a5369cb5428b85829ef0895b72c218bea39
semih gümüş <semihgms65@gmail.com>
2025-01-28 14:21:08 +0300

    Migrationlar eklendi

---
+++ database/migrations/2025_01_28_103654_create_courses_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title'); // Ders adı
+            $table->text('description'); // Ders açıklaması
+            $table->string('drive_link'); // Google Drive linki
+            $table->string('image')->nullable(); // Blog resmi (opsiyonel)
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
